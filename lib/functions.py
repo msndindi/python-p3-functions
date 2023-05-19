d28856157baaf481351cb86388eadd1eb8af4ef9
Mutindi Hope <msndindi@outlook.com>
2023-05-19 16:26:02 +0300

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print ("Hello, programmer!")
     pass
 
 def greet(name):
+    print (f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
+    if isinstance(number, (int, float)):
+        return number / 2
+    else:
+        return None
     pass
